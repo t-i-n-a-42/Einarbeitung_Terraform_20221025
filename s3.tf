b377d37d5071f10d0b0b3126c7106554e619ce2c
t-i-n-a-42 <marek.42@icloud.com>
2022-12-27 18:52:27 +0100

    neuer bucket 3

--- s3.tf
+++ s3.tf
@@ -5,3 +5,7 @@ resource "aws_s3_bucket" "first_bucket" {
 resource "aws_s3_bucket" "second_bucket" {
   bucket = "my-tf-test-bucket-mab2"
 }
+
+resource "aws_s3_bucket" "third_bucket" {
+  bucket = "my-tf-test-bucket-mab3"
+}
