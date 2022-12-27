12272416225b1fd386dafc0e6b5274a50119d99c
t-i-n-a-42 <marek.42@icloud.com>
2022-12-27 22:47:21 +0100

    V81

--- terraform_config/s3.tf
+++ terraform_config/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "first_bucket" {
-  bucket = "my-tf-test-bucket-mab_A01"
+  bucket = "my-tf-test-bucket-mab-A01"
 }
 
 resource "aws_s3_bucket" "secunda_bucket" {
-  bucket = "my-tf-test-bucket-mab_A02"
+  bucket = "my-tf-test-bucket-mab-A02"
 }
