115c23e7066cdefe975c046dfcc8ecc44f0d7b8d
t-i-n-a-42 <marek.42@icloud.com>
2022-12-27 20:39:40 +0100

    versuch 2

--- terraform_config/s3.tf
+++ terraform_config/s3.tf
@@ -2,10 +2,6 @@ resource "aws_s3_bucket" "first_bucket" {
   bucket = "my-tf-test-bucket-mab1"
 }
 
-resource "aws_s3_bucket" "second_bucket" {
-  bucket = "my-tf-test-bucket-mab2"
-}
-
 resource "aws_s3_bucket" "third_bucket" {
   bucket = "my-tf-test-bucket-mab3"
 }
